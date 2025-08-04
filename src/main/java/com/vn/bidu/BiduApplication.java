5e206e72f3bdaf5e23a183473b1c8557f0f29c91
Pham Dang Duan <dangduan210605@gmail.com>
2025-08-04 18:03:53 +0700

    Initial commit

---
+++ src/main/java/com/vn/bidu/BiduApplication.java
@@ -0,0 +1,13 @@
+package com.vn.bidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vn/bidu/BiduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.bidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
